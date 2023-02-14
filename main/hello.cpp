7d9a2d66a9922a1f16d4871b4b83f00ff8c8d7e5
Abhishek Patil <abhijpatil2003@gmail.com>
2023-02-14 08:56:57 +0530

    first commit

---
+++ main/hello.cpp
@@ -0,0 +1,12 @@
+// File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+  cout << "Hello, World!\n";
+  cout << "Hello, Jenkins!\n";
+  cout << "PES1UG20CS010 Here.\n";
+  cout << "I have successfully built and run.\n";
+  return 0;
+}
